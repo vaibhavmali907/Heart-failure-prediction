fdfbdf7bb87525962118c401e0cde287393ae2d0
vaibhav mali <67496777+vaibhavmali907@users.noreply.github.com>
2022-07-15 13:45:58 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -6,7 +6,7 @@ sc = pickle.load(open('sc.pkl', 'rb'))
 class_names = [0,1]
 
 def predict(df):
-    df = df[['age','anaemia','creatinine_phosphokinase','diabetes','ejection_fraction','high_blood_pressure','platelets','serum_creatinine','serum_sodium','sex','smoking','time']]
+    df = df[['ejection_fraction','serum_creatinine','serum_sodium','time']]
     df = sc.transform(df)
     predictions = model.predict(df)
     output = [class_names[class_predicted] for class_predicted in predictions]
